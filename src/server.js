f047c9931105161404aa9cdd63ddb0725a7f1e8d
jeison Pedroso <jeison.contas@gmail.com>
2019-10-14 22:24:04 -0300

    back atualizado

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const routes = require('./routes');
 const mongoose = require('mongoose');
-
+const cors = require('cors')
+const path = require('path');
 //req.query.idade => pega parametro no get ou seja na url
 //req.params.id => acessar route parans para update e delete
 //req.body
@@ -13,7 +14,9 @@ mongoose.connect('mongodb+srv://oministack:oministack@airbnb-cbu19.mongodb.net/a
     useNewUrlParser:true,
     useUnifiedTopology:true
 });
+app.use(cors());
 app.use(express.json());
+app.use('/files',express.static(path.resolve(__dirname,'..','uploads')));
 app.use(routes);
 
 app.listen('555');
